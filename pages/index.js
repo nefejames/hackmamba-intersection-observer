5718df3705cebed5300fa394dee5c42cd2b4f846
nefejames <nefejames@yahoo.com>
2022-03-21 18:19:34 +0000

    fixed?

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,21 @@
+import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import { InView } from "react-intersection-observer";
 
-const data = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-];
+function ImpressionCounter(inView, i, count, setCount) {
+  if (inView) {
+    setCount((count) => count + 1);
+    console.log(`image ${i + 1} in view`);
+    console.log(count);
 
-function ImpressionCounter(inView, entry, i) {
-  if (i == data[i]) {
-    console.log(`image ${i} in view`);
+    localStorage.setItem("impressionCount", count);
   }
-
-  console.log(data[i]);
-
-  // console.log(entry);
-
-  // console.log(i);
 }
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="w-full h-full flex flex-col justify-center items-center text-center pt-10 bg-zinc-900">
       <Head>
@@ -29,16 +26,16 @@ export default function Home() {
         Next.js Intersection Observer
       </h1>
 
-      {data.map((i) => (
+      {Array.from(Array(20).keys()).map((i) => (
         <InView
           key={i}
           threshold={1}
-          onChange={(inView, entry) => ImpressionCounter(inView, entry, i)}
+          onChange={(inView) => ImpressionCounter(inView, i, count, setCount)}
         >
-          {({ ref, inView, entry }) => (
+          {({ ref, inView }) => (
             <div className="mb-10" key={i} ref={ref}>
               <Image
-                src={`/images/${i}.png`}
+                src={`/images/${i + 1}.png`}
                 width={300}
                 height={400}
                 alt="something amazing"
